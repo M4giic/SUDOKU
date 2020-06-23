843275782e2c2898008ba8e9f376e18aa1dee022
M4giic <66871586+M4giic@users.noreply.github.com>
2020-06-23 19:10:11 +0200

    Base_code
    This is basic game_app.c with most imporant functions. Printing sudoku and assiging values. Current assigment is random but is used to verify if print is wokring correctly

---
+++ game_app.c
@@ -0,0 +1,170 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+//structure visualation
+/*
+
+     BIGsqr   |   BIGsqr   |  BIGsqr  
+   [smallsqr] | [smallsqr] | [smallsqr]
+  --------------------------------------
+     BIGsqr   |   BIGsqr   |   BIGsqr  
+   [smallsqr] | [smallsqr] | [smallsqr]
+  --------------------------------------
+     BIGsqr   |   BIGsqr   |   BIGsqr  
+   [smallsqr] | [smallsqr] | [smallsqr]
+
+*/
+
+//program fucntionality
+/*
+
+  1. Create a correct reference SUDOKU_ORIGIN
+  2. Copy values and create SUDOKU_SOLUTION
+  3. Remove a number of values from SUDOKU_SOLUTION 
+  4. Start solving loop
+    1) while loop with "bool solved" as variable
+    2) show current SUDOKU_SOLUTION 
+    3) ask for BIGsqr position
+    4) ask for SMALLsqr data index
+    5) ask for value to be placed
+    6) check if new SUDOKU_SOLUTION value is same as SUDOKU_ORIGIN
+        a) if ok add it to SUDOKU_SOLUTION
+        b) if not, say "you dumb bitch"
+    7) if not solved go back to point 2
+*/
+
+//TO DO and improvment
+/*
+    TO DO LIST: 
+        1) everything
+
+    POTENTIAL IMPROVEMNT
+        1) create SUDOKU_ORIGIN generation algorythm
+*/
+
+
+typedef struct SMALLsqr
+{
+int data[3][3];
+int solved;
+
+}SMALLsqr;
+
+typedef struct BIGsqr
+{
+SMALLsqr content;
+
+int posX;
+int posY;
+
+}BIGsqr;
+
+typedef struct SUDOKU
+{
+    BIGsqr GRID[3][3];
+   
+}SUDOKU;
+/*
+void pos_allocation(SUDOKU* SUDOKU_temp)
+{
+    SUDOKU_temp->BIGsqr11.posX = 1;
+    SUDOKU_temp->BIGsqr11.posY = 1;
+
+    SUDOKU_temp->BIGsqr12.posX = 1;
+    SUDOKU_temp->BIGsqr12.posY = 2;
+
+    SUDOKU_temp->BIGsqr13.posX = 1;
+    SUDOKU_temp->BIGsqr13.posY = 3;
+
+    SUDOKU_temp->BIGsqr21.posX = 2;
+    SUDOKU_temp->BIGsqr21.posY = 1;
+    
+    SUDOKU_temp->BIGsqr22.posX = 2;
+    SUDOKU_temp->BIGsqr22.posY = 2;
+
+    SUDOKU_temp->BIGsqr23.posX = 2;
+    SUDOKU_temp->BIGsqr23.posY = 3;
+
+    SUDOKU_temp->BIGsqr31.posX = 3;
+    SUDOKU_temp->BIGsqr31.posY = 1;
+    
+    SUDOKU_temp->BIGsqr32.posX = 3;
+    SUDOKU_temp->BIGsqr32.posY = 2;
+
+    SUDOKU_temp->BIGsqr33.posX = 3;
+    SUDOKU_temp->BIGsqr33.posY = 3;
+}
+*/
+void print_sudoku(SUDOKU* SUDOKU_temp)
+{
+
+    //this function will always print entire sudoku onto screen
+
+
+    for (int index_sqrY=0; index_sqrY < 3; index_sqrY++)
+    {
+        for (int index_row = 0; index_row < 3; index_row++)
+        {
+            for (int index_sqrX=0; index_sqrX < 3; index_sqrX++)
+            {
+                for (int index_column=0; index_column < 3; index_column++)
+                {
+                    printf(" %d",SUDOKU_temp->GRID[index_sqrY][index_sqrX].content.data[index_row][index_column]);
+                }
+                printf("|");
+                
+            }
+            printf("\n");
+        }
+        for (size_t i = 1; i < 12; i++)
+        printf("--");
+        printf("\n");
+    }
+
+    printf(" \n -------------------- \n This is Your sudoku \n\n");
+    
+}
+void assignvalue_sudoku(SUDOKU* SUDOKU_temp)
+{
+
+    for (int index_sqrY=0; index_sqrY < 3; index_sqrY++)
+    {
+        for (int index_row = 0; index_row < 3; index_row++)
+        {
+            for (int index_sqrX=0; index_sqrX < 3; index_sqrX++)
+            {
+                for (int index_column=0; index_column < 3; index_column++)
+                {
+                    //this can generate values to be asigned to slots
+
+                    SUDOKU_temp->GRID[index_sqrY][index_sqrX].content.data[index_row][index_column]=1+index_column+index_sqrX+index_row+index_sqrY;
+                    //printf("current values: \n\n index_column = %d \n index_row = %d \n index_sqrX = %d \n index_sqrY = %d \n",index_column,index_row,index_sqrX,index_sqrY);
+                    //printf("suma = %d \n\n",1+index_column+index_sqrX+index_row+index_sqrY);
+                }
+                
+            }
+            
+        }
+
+    }
+
+    printf(" \n ---------------- \n Values assigned\n\n\n");
+   
+
+}
+
+
+int main (int argc, char* argv[])
+{
+    //creating SUDOKU ORIGIN
+
+    SUDOKU SUDOKU_ORIGIN, SUDOKU_SOLUTION;
+
+    assignvalue_sudoku(&SUDOKU_ORIGIN);
+    print_sudoku(&SUDOKU_ORIGIN);
+
+    //pos_allocation(&SUDOKU_ORIGIN);
+    //pos_allocation(&SUDOKU_SOLUTION);
+
+    return 0;
+}
